47a056f0ad24187b661d9c4842b186f5dd12a82d
Michael Pankov <work@michaelpankov.com>
2015-12-07 14:32:49 +0300

    Consider crate binary if there's main.rs
    This way, at least simplest binary crate flychecking works with modules.
    
    Currently, flycheck works ok in src/main.rs as it considers the crate binary while you're in that file. When working in src/foo.rs, which is a module foo of same binary crate, current setup's crate type guessing logic breaks, as modules are not accounted for at all.
    
    This makes crate type guessing work in "global" fashion: now flycheck-rust-crate-type is "bin" in every file that belongs to this crate, if src/main.rs exists.

--- flycheck-rust.el
+++ flycheck-rust.el
@@ -109,7 +109,10 @@ Flycheck according to the Cargo project layout."
                     (not (flycheck-rust-executable-p rel-name)))
         ;; Set the crate type
         (setq-local flycheck-rust-crate-type
-                    (if (flycheck-rust-executable-p rel-name) "bin" "lib"))
+                    (if
+                        (file-exists-p
+                         (expand-file-name "src/main.rs" flycheck-rust-crate-root))
+                        "bin" "lib"))
         ;; Find build libraries
         (setq-local flycheck-rust-library-path
                     (list (expand-file-name "target/debug" root)

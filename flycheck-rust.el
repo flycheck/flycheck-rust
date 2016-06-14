cbcdf416d8e0aab8c6208cbb758c30a6d6063f9f
Sebastian Wiesner <swiesner@lunaryorn.com>
2016-06-14 20:30:04 +0200

    Add missing dependency on let-alist
    Fixes GH-27

--- flycheck-rust.el
+++ flycheck-rust.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/flycheck/flycheck-rust
 ;; Keywords: tools, convenience
 ;; Version: 0.1-cvs
-;; Package-Requires: ((emacs "24.1") (flycheck "0.20") (dash "2.4.0") (seq "2.15"))
+;; Package-Requires: ((emacs "24.1") (flycheck "0.20") (dash "2.4.0") (seq "2.15") (let-alist "1.0.4"))
 
 ;; This file is not part of GNU Emacs.
 

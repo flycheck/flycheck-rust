75c4e039968159d031787d940482097f4cdab81d
Anders Kaseorg <andersk@mit.edu>
2017-04-02 03:17:37 -0400

    Pass --format-version 1 to cargo metadata
    cargo 0.19.0-nightly otherwise complains to stderr:
    
    warning: please specify `--format-version` flag explicitly to avoid
    compatibility problems
    
    We should heed this warning.  Additionally, since we tell call-process
    to mix stdout and stderr into the same buffer, this warning’s presence
    was confusing json-read.
    
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- flycheck-rust.el
+++ flycheck-rust.el
@@ -80,9 +80,10 @@ root of the file hierarchy."
 
 MANIFEST is the path to the Cargo.toml file of the project.
 
-Calls `cargo metadata --no-deps --manifest MANIFEST', parses and
-collects the targets for the current workspace, and returns them
-in a list, or nil if no targets could be found."
+Calls `cargo metadata --no-deps --manifest-path MANIFEST
+--format-version 1', parses and collects the targets for the
+current workspace, and returns them in a list, or nil if no
+targets could be found."
   (let ((cargo (funcall flycheck-executable-find "cargo")))
     (unless cargo
       (user-error "flycheck-rust cannot find `cargo'.  Please \
@@ -95,7 +96,8 @@ more information on setting your PATH with Emacs."))
                              (with-temp-buffer
                                (call-process cargo nil t nil
                                              "metadata" "--no-deps"
-                                             "--manifest-path" manifest)
+                                             "--manifest-path" manifest
+                                             "--format-version" "1")
                                (goto-char (point-min))
                                (let ((json-array-type 'list))
                                  (json-read)))
